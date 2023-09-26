f04f19c1ddca959b92b533a76d8ab5802fb1a492
Mac <ajeemmanuel221@gmail.com>
2023-09-26 22:00:16 +0100

    further development

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { React, useState } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Started a new proejct</h1>
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
     </div>
   )
 }
